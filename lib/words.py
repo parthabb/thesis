7b2656251242855e26e2b71f9b0b9f077468836c
Partha Bijoy Baruah <parthabb@gmail.com>
2015-03-31 14:01:00 -0500

    partial code for finding words

--- lib/words.py
+++ lib/words.py
@@ -70,18 +70,65 @@ class Words(lib.Singleton):
             count += len(probable_words.get(dis))
         return probable_words
 
-    def _trie_util(self, trie, word):
+    def _trie_util(self, trie, word, flipped_bit_index):
         """Checks if the word is in the trie."""
         curr = trie
+        flipped_node = trie
+        count = 0
         for bit in word:
             if bit == '0':
                 curr = curr.left
             else:
                 curr = curr.right
+            if flipped_bit_index != None:
+                if count == flipped_bit_index:
+                    flipped_node = curr
+            count += 1
             if not curr:
                 break
-        return curr != None
+        return curr != None, flipped_node
+
+    def _bit_flip_word_generator(self, k, trie, error_word):
+        """Return all valid words after 0 to k bit flips."""
+        probable_words = {}
+        isword, flipped_node = self._trie_util(trie, error_word, None)
+        if isword:
+            probable_words.update({0: [error_word]})
+        bit_flip_list = [('', trie, error_word)]
+        for prev, root, rest in bit_flip_list:
+            stream = list(rest)
+            stream_len = len(stream)
+            temp_bit_flip_list = []
+            for m in range(stream_len):
+                temp = list(stream)
+                if temp[m] == '0':
+                    temp[m] = '1'
+                else:
+                    temp[m] = '0'
+                temp = ''.join(temp)
+                isword, flipped_node = self._trie_util(root, temp, m)
+                if isword:
+                    words_list = probable_words.get(1, [])
+                    words_list.append(temp)
+                    probable_words[1] = words_list
+                    temp_bit_flip_list.append((temp[:m], flipped_node, temp[m + 1:]))
+        bit_flip_stream = temp_bit_flip_list
+
+
+
+        for n in xrange(1, k + 1):
+            for 
+            index_dict = {}
+            stream = list(error_word)
+            
+            bit_flip_dict[n] = index_dict
 
     def get_most_probable_words_through_trie(self, error_word):
         """Given a word with bit errors, get the most probable words."""
+        word_len = len(error_word)
+        trie = self._encoded_tries_by_length.get(word_len)
+        probable_words = {}
+        if self._trie_util(trie, error_word):
+            probable_words.update({0: error_word})
+
         pass
